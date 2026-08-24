5942a3d3eb82c7adbc5194048001fd3a532a9a2a
Skip <skip@usecurling.com>
2026-08-24 19:46:37 -0300

    CORRIGIR: O hook ping.js (pocketbase/hooks/ping.js) está implantado mas…

--- pocketbase/hooks/ping.js
+++ pocketbase/hooks/ping.js
@@ -1,5 +1,11 @@
 console.log('ping hook loaded')
 
+onBootstrap((e) => {
+  console.log('ping onBootstrap called')
+  e.next()
+})
+
 routerAdd('GET', '/api/custom/ping', (c) => {
+  console.log('ping endpoint called')
   return c.json(200, { ok: true, time: new Date().toISOString() })
 })
